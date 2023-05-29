96aee0b8e4d012c08f1af20d1f154b856e8288fa
Ben Medcalf <benmedcalf@macbook-pro.myfiosgateway.com>
2023-05-28 23:40:18 -0400

    very wip - calling to db successfully tho

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3001;
+const { MongoClient } = require("mongodb");
 
 // cors
 app.use(cors({ origin: true, credentials: true }));
@@ -9,12 +10,56 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
+// Connection URL
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+const dbName = "mtg-collection";
+// Use connect method to connect to the server
+
+client.connect();
+
+console.log("Connected successfully to server");
+const db = client.db(dbName);
+const cardCollection = db.collection("cardstwo");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/scryfall-card/:id", (req, res) => {
-  res.json({ id: req.params.id });
+// TODO: Move to own file
+const basicCardDetailsProjection = {
+  name: 1,
+  type_line: 1,
+  cmc: 1,
+  colors: 1,
+  rarity: 1,
+  power: 1,
+  toughness: 1,
+  oracle_text: 1,
+  set_name: 1,
+  color_identiy: 1,
+  keywords: 1,
+  image_uris:1,
+  prices: 1
+};
+
+app.get("/scryfall-card/:name", async (req, res) => {
+  console.log(req.params.name)
+  let mongoQuery = {};
+  mongoQuery.name = {
+    $regex: req.params.name,
+    $options: "i",
+  };
+  try {
+    console.log(mongoQuery)
+    let cards = await cardCollection
+      .find(mongoQuery, { projection: basicCardDetailsProjection })
+      .toArray();
+    res.send(cards);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 app.listen(port, () => {
