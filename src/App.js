23eaba753ef39a1ffcbeff91b0de2b0a4c5eb304
Ben Medcalf <benmedcalf@macbook-pro.myfiosgateway.com>
2023-05-30 21:26:19 -0400

    wip - displaying data from db

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
-import { SearchBar } from "./SearchBar";
 
-export const getCards = (card) =>
-  fetch(`http://localhost:3001/scryfall-card/${card}`)
-    // get it
-    .then((res) => {
-      // read it
-      res.json().then((body) => {
-        console.log(body[0]);
-      });
-    })
-    .catch((err) => console.log(`error! ${err}`));
-
-const SearchResult = (props) => {
-
-  const [result, setResult] = useState({ name: "testing", id: "id" });
-
-  useEffect(() => {
-    console.log("using effect");
-    fetch(`http://localhost:3001/scryfall-card/${props.query}`)
+const Search = () => {
+  const getCards = (card) =>
+    fetch(`http://localhost:3001/scryfall-card/${card}`)
       // get it
       .then((res) => {
         // read it
         res.json().then((body) => {
+          handleResult(body);
           console.log(body);
-          body.forEach((i) => console.log(i.name));
-          setResult(body[0]);
         });
       })
       .catch((err) => console.log(`error! ${err}`));
-  }, [result]);
+
+  const [cards, setCards] = useState([]);
+  function handleResult(result) {
+    setCards(result);
+  }
+
+  const [searchTerm, setSearchTerm] = useState("Thrashing Brontodon");
+  const handleSearchTerm = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <div>
-      <p>{result.name}</p>
-      <p>{result.id}</p>
+      <input
+        type="text"
+        placeholder="Search Here"
+        onChange={handleSearchTerm}
+        value={searchTerm}
+      />
+      <button
+        onClick={() => {
+          getCards(searchTerm);
+        }}
+      >
+        Search
+      </button>
+      <ul>
+        {cards.map((card) => (
+          <li key={card._id}>
+            <img src={card.image_uris.small} />
+            <div>
+              <p>{card.name}</p>
+              <p>${card.prices.usd}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
@@ -46,8 +60,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="../Magic_card_back.webp" className="App-logo" alt="logo" />
-        <SearchBar />
-        <SearchResult query="Thrashing" />
+        <Search query="Thrashing" />
       </header>
     </div>
   );
